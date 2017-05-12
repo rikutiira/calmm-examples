a61810d25b700a60838ef012d770dd098d45b43d
Riku Tiira <riku@tiira.net>
2017-05-12 11:36:17 +0300

    Updated Assignment instructions

--- src/views/Assignment.jsx
+++ src/views/Assignment.jsx
@@ -1,8 +1,10 @@
 /**
  * Write a component that:
- * - Has two number inputs
+ * - Has two text inputs
  * - If the sum of the two inputs is 42, display text "Done"
  * - Use the following functions: U.atom, U.add, U.ift, U.equals
+ * - Extra if time:
+ *      - show a warning if input values are non-integers (decimals, not numbers...)
  */
 
 const Assignment = () => {
